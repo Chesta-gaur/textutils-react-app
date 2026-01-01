cd510dc4cf259af438855cdd37078b33633a9102
Chesta-gaur <chestagaur004@gmail.com>
2026-01-01 16:19:58 +0530

    Fix dark mode logic and sync App and TextForm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import "./App.css";
 import TextForm from "./components/TextForm";
 
 function App() {
+  const [mode, setMode] = useState("Light Mode");
+
+  const toggleMode = () => {
+    if (mode === "Light Mode") {
+      setMode("Dark Mode");
+      document.body.style.backgroundColor = "black";
+    } else {
+      setMode("Light Mode");
+      document.body.style.backgroundColor = "white";
+    }
+  };
+
   return (
-    <div className="container">
-      <TextForm heading="Enter the text to analyze..." />
-    </div>
+    <>
+      <div className="container">
+        <TextForm
+          heading="Enter the text to analyze..."
+          mode={mode}
+          toggleMode={toggleMode}
+        />
+      </div>
+    </>
   );
 }
 
